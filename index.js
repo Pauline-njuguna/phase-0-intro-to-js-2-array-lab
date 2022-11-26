1b1330abef0e73f7d9db6900fd46f99f6d2879e5
pauline-njunguna <paulinenjunguna.w@gmail.com>
2022-11-26 04:29:36 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+cats.length = 0;
+cats.push("Milo", "Otis", "Garfield");
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  const cats_app = [...cats, name];
+  return cats_app;
+}
+
+function prependCat(name) {
+  const cats_prep = [name, ...cats];
+  return cats_prep;
+}
+
+function removeLastCat() {
+  const cats_last = cats.slice(0, -1);
+  return cats_last;
+}
+
+function removeFirstCat() {
+  const cats_first = cats.slice(1);
+  return cats_first;
+}
+console.log(removeFirstCat);
